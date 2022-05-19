0142371c0f8e113647a2926c7eb23b1f73b09f7c
sumin-yang224 <kzx1547p@gmail.com>
2022-05-19 20:17:45 +0900

    [create] Product Model 생성

--- product/models.py
+++ product/models.py
@@ -1,3 +1,16 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    pid = models.PositiveIntegerField(primary_key=True) # Inter크기
+    name = models.CharField(max_length=50)
+    kinds = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    product_img_url = models.CharField(max_length=200)
+    convenience_store =models.CharField(max_length=50)
+    event_type = models.CharField(max_length=50) 
+    
+
+
