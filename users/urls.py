4e0d1b71828061aa1e0ce789e61ca0214a59fbfa
sumin-yang224 <kzx1547p@gmail.com>
2022-06-30 02:08:54 +0900

    [update] Profile 설정

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from .views import SignUpView, SignInView
 urlpatterns = [
     path('signup/', SignUpView.as_view()),
     path('signin/', SignInView.as_view()),
+    path('profile/<int:pk>/',ProfileView.as_view()),
 ]
--- wowstore/urls.py
+++ wowstore/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('product/', include('product.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
