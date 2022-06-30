308280451cd0e3f13e001131f0b6d4435227c86d
sumin-yang224 <kzx1547p@gmail.com>
2022-06-30 19:44:55 +0900

    [fix] 수정

--- users/models.py
+++ users/models.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class Users(AbstractUser):
-    pass
-
 class MyUserManager(BaseUserManager):
     # overwriting
     def create_superuser(self, phonenumber, password=None):
